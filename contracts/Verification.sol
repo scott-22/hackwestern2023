9566c03fb2581083a99685611a8cdc98a761d1c6
Irisimoo <irismo1009@gmail.com>
2023-11-25 09:20:20 -0500

    update verification

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -4,7 +4,7 @@ contract Verification {
     address public cra;
     
     struct User {
-      bool verStatus;
+      bool isVerified;
       string userInfo;
     }
     mapping(address => User) public users;
@@ -13,17 +13,17 @@ contract Verification {
 constructor() {
   cra = msg.sender;
 
-  users[msg.sender].verStatus = true;
+  users[msg.sender].isVerified = true;
   users[msg.sender].userInfo = "cra";
 }
 
 // Retrieving the verStatus
-function getVerStatus() public view returns (bool) {
-  return users[msg.sender].verStatus;
+function getIsVerified() public view returns (bool) {
+  return users[msg.sender].isVerified;
 }
 
 // Retrieving the enid
-function getEnid() public view returns (string memory) {
+function getUserInfo() public view returns (string memory) {
   return users[msg.sender].userInfo;
 }
 
@@ -33,7 +33,7 @@ function verify() public {
     msg.sender == cra,
     "Only cra has right to verify"
   );
-  users[msg.sender].verStatus = true;
+  users[msg.sender].isVerified = true;
 }
 
 /* Adopting a pet
