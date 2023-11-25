7c9bde92bbb226c0861665141a1c4986ceed9df2
Feng <yunfengzhang789@gmail.com>
2023-11-25 11:27:17 -0500

    fixed duplicate router definition

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import { useState, useEffect, useCallback } from 'react';
 import {
   BrowserRouter as Router,
-  Router,
   Route,
   Routes
 } from "react-router-dom";
