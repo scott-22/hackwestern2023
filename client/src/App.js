1060d2531ea7216824a3c4aff346f365fa3f9908
scott-22 <scotthao65@gmail.com>
2023-11-24 23:36:25 -0500

    install tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 import { useState, useEffect, useCallback } from 'react';
 
 function App() {
@@ -22,8 +21,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className=''>
           Test message: {testVar}
         </p>
         
