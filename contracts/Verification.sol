70f90460076793a8336c6abe4bbd05b1c865ac82
Irisimoo <irismo1009@gmail.com>
2023-11-25 02:04:32 -0500

    map user:verified (address => bool)

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -1,8 +1,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Verification {
-    address[16] public adopters;
+    mapping(address => bool) public verStatus;
+
+    // address[16] public adopters;
     // Adopting a pet
+
+// 
+constructor() {
+  verStatus[msg.sender] = false;
+}
+
+// Retrieving the verStatus
+function getVerStatus() public view returns (bool) {
+  return verStatus[msg.sender];
+}
+
+// Setting the verStatus
+function setVerStatus(bool newStatus) public {
+  verStatus[msg.sender] = newStatus;
+}
 
 // Adopting a pet
 function adopt(uint petId) public returns (uint) {
@@ -11,11 +28,6 @@ function adopt(uint petId) public returns (uint) {
   adopters[petId] = msg.sender;
 
   return petId;
-}
-
-// Retrieving the adopters
-function getAdopters() public view returns (address[16] memory) {
-  return adopters;
 }
 
 }
